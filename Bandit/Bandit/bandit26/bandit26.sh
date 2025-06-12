7fd377e159acbcfe9fb1b4546ae835638817481c
lnn0v4 <75952645+lnn0v4@users.noreply.github.com>
2025-06-12 13:09:39 -0500

    Update bandit26.sh

--- Bandit/Bandit/bandit26/bandit26.sh
+++ Bandit/Bandit/bandit26/bandit26.sh
@@ -3,7 +3,7 @@
 fichero=$(find / -type f -name bandit27-do 2>/dev/null)
 
 if [ -s $fichero ];then
-        cat "/etc/bandit_pass/bandit26"
+        cat "/etc/bandit_pass/bandit27"
 else
         echo "No existe el fichero"
 fi
